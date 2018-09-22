4d063e80b554b10723352e61754fa75676418274
rafaelpllopes <rafael.pllopes@gmail.com>
2018-09-22 15:26:15 -0300

    Criado os models e middlewares e boot para organizar o projeto.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import express from "express";
 import consign from "consign";
 
-const PORT = 3000;
 const app = express();
 
-app.set("json spaces", 4);
-
 consign()
-    .include('routes')
+    .include('models')
+    .then('libs/middlewares.js')
+    .then('routes')
+    .then('libs/boot.js')
     .into(app);
-
-app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`));

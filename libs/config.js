f677e81c7b02f5619b74992eaa71172e78b93620
rafaelpllopes <rafael.pllopes@gmail.com>
2018-09-22 17:55:20 -0300

    Criado a integração com o banco de dados.

---
+++ libs/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    database: 'ntask',
+    username: '',
+    password: '',
+    params: {
+        dialect: 'sqlite',
+        storege: 'ntask.sqlite',
+        define: {
+            underscored: true
+        }
+    }
+};
